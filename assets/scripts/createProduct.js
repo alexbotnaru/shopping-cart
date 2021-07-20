e7a7bd7b73b9a91417293604744f7be83f1a91de
abotnaru <alexandru.botnaru@isa.utm.md>
2021-07-20 13:21:07 +0300

    work in progress

---
+++ assets/scripts/createProduct.js
@@ -0,0 +1,35 @@
+const saveButton = document.querySelector('.save-btn');
+const title = document.querySelector('#title');
+const description = document.querySelector('#description');
+const price = document.querySelector('#price');
+const image = document.querySelector('#image');
+
+let productsList = [];
+
+if (localStorage.getItem('product')) {
+    productsList = JSON.parse(localStorage.getItem('product'));
+}
+
+saveButton.addEventListener('click', function (){
+    if (!(title.value || description.value || price.value)) return;
+    let newProduct = {
+        title: title.value,
+        description: description.value,
+        price: price.value,
+        image: image.value || 'no-image.jpg'
+    };
+
+    productsList.push(newProduct);
+    localStorage.setItem('product', JSON.stringify(productsList));
+    title.value = '';
+    description.value = '';
+    price.value = '';
+    image.value = '';
+
+    // let confirmation = document.createElement('p');
+    // confirmation.innerText = 'Product created!';
+    // confirmation.style.color = '#60e74d';
+    // document.body.append(confirmation);
+    //@fixme element disappears
+    alert('Product created!');
+});
---
+++ assets/scripts/getAllProducts.js
@@ -0,0 +1,40 @@
+const productCard = document.querySelector('.product-card');
+
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
+
+if (localStorage.getItem('product')) {
+    productsList = JSON.parse(localStorage.getItem('product'));
+    displayProducts();
+}
+
+function displayProducts() {
+    let displayProduct = '';
+
+    productsList.forEach((item, i) => {
+        if (i % 4 === 0) {
+            displayProduct += `<div class="row">`;
+        }
+
+        displayProduct +=
+            `
+            <div class="card col-md-4 col-6 col-lg-3 mt-4 col">
+                <img src="assets/img/${item.image}" class="card-img-top" alt="${item.image}">
+                <div class="card-body">
+                    <h5 class="card-title">${item.title}</h5>
+                    <p class="card-text">${item.description}<br> ${item.price}$</p>
+                    <a href="#" class="btn btn-primary">More details</a> <span> <a href="#" class="btn btn-danger btn-sm">Add to cart</a></span>
+                </div>
+            </div>
+       ` ;
+
+        if (i % 4 === 3) {
+            displayProduct += `</div>`
+        }
+
+        productCard.innerHTML = displayProduct;
+
+    });
+
+}
